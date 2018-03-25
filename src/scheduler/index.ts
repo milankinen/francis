74836717d4eddf42ddc6f0346143930ea46ad267
Matti Lankinen <m.lankinen@iki.fi>
2018-03-25 15:41:55 +0300

    Add way to override the default scheduler

--- src/scheduler/index.ts
+++ src/scheduler/index.ts
@@ -3,8 +3,32 @@ import { Scheduler } from "./Scheduler"
 
 export { Scheduler, Task, Timeout, OnTimeout } from "./Scheduler"
 
-const scheduler = new DefaultScheduler()
+const DEFAULT = new DefaultScheduler()
+let scheduler: Scheduler = DEFAULT
+let isOverride = false
 
 export function currentScheduler(): Scheduler {
   return scheduler
+}
+
+export function withScheduler<S extends Scheduler>(
+  impl: S,
+  block: (scheduler: S, done: () => void) => void,
+): void {
+  if (isOverride) {
+    throw new Error("Nested withScheduler not supported")
+  }
+  const prev = scheduler
+  if (prev.hasPendingTasks()) {
+    throw new Error("Previous scheduler has still tasks pending")
+  }
+  scheduler = impl
+  isOverride = true
+  block(impl, () => {
+    if (scheduler.hasPendingTasks()) {
+      throw new Error("Override scheduler has still tasks pending")
+    }
+    isOverride = false
+    scheduler = prev
+  })
 }
