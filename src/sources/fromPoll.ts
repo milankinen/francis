fa486c49cec993a0d65d914e4941fd05529545bc
Matti Lankinen <m.lankinen@iki.fi>
2018-03-28 20:03:38 +0300

    Fix fromPoll and sequentially typings

--- src/sources/fromPoll.ts
+++ src/sources/fromPoll.ts
@@ -1,19 +1,21 @@
 import { AnyEvent } from "../bacon"
+import { isEvent, Next } from "../Event"
 import { EventStream } from "../EventStream"
 import { identity } from "../operators/_base"
 import { TimerBase } from "./_timer"
 
-export function fromPoll<T>(interval: number, f: () => AnyEvent<T>): EventStream<any> {
+export function fromPoll<T>(interval: number, f: () => T | AnyEvent<T>): EventStream<any> {
   return new EventStream(identity(new FromPoll(interval, f)))
 }
 
 class FromPoll<T> extends TimerBase<T> {
-  constructor(interval: number, private f: () => AnyEvent<T>) {
+  constructor(interval: number, private f: () => T | AnyEvent<T>) {
     super(interval)
   }
 
   public tick(): Array<AnyEvent<T>> {
     const f = this.f
-    return [f()]
+    const next = f()
+    return [isEvent(next) ? next : new Next(next)]
   }
 }
--- src/sources/sequentially.ts
+++ src/sources/sequentially.ts
@@ -1,47 +1,36 @@
 import { AnyEvent } from "../_interfaces"
-import { End, Next } from "../Event"
+import { End, isEvent, Next } from "../Event"
 import { EventStream } from "../EventStream"
 import { identity } from "../operators/_base"
 import { TimerBase } from "./_timer"
 
-export function sequentially<T>(interval: number, events: T[]): EventStream<T>
-export function sequentially<A, B>(interval: number, events: [A, B]): EventStream<A | B>
-export function sequentially<A, B, C>(interval: number, events: [A, B, C]): EventStream<A | B | C>
-export function sequentially<A, B, C, D>(
-  interval: number,
-  events: [A, B, C, D],
-): EventStream<A | B | C | D>
-export function sequentially<A, B, C, D, E>(
-  interval: number,
-  events: [A, B, C, D, E],
-): EventStream<A | B | C | D | E>
-export function sequentially<A, B, C, D, E, F>(
-  interval: number,
-  events: [A, B, C, D, E, F],
-): EventStream<A | B | C | D | E | F>
-export function sequentially(interval: number, events: any[]): EventStream<any> {
-  return _sequentially(interval, events)
-}
-
-export function _sequentially(interval: number, events: any[]): EventStream<any> {
+export function sequentially<T>(interval: number, events: Array<T | AnyEvent<T>>): EventStream<T> {
   return new EventStream(identity(new Sequentially(interval, events)))
 }
 
-class Sequentially extends TimerBase<any> {
+class Sequentially<T> extends TimerBase<T> {
   private i: number
-  constructor(interval: number, private items: any[]) {
+  constructor(interval: number, private items: Array<T | AnyEvent<T>>) {
     super(interval)
     this.i = 0
   }
 
-  public tick(): Array<AnyEvent<any>> {
+  public tick(): Array<AnyEvent<T>> {
     switch (this.items.length - this.i) {
       case 0:
         return [new End()]
-      case 1:
-        return [new Next(this.items[this.i++]), new End()]
-      default:
-        return [new Next(this.items[this.i++])]
+      case 1: {
+        const it = this.items[this.i++]
+        if (isEvent(it)) {
+          return it.isEnd ? [it] : [it, new End()]
+        } else {
+          return [new Next(it), new End()]
+        }
+      }
+      default: {
+        const it = this.items[this.i++]
+        return [isEvent(it) ? it : new Next(it)]
+      }
     }
   }
 }
